769363ac6bd277a11900b57bd9bdc02f8f4115fd
王国平 <@WANG>
2016-10-07 14:40:37 +0800

    添加项目文件。

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+
+
+namespace WebApplication4.Controllers
+{
+
+
+    public class HomeController : Controller
+    {
+        MySqlConnection conn = null;
+
+     // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.a = "初始化";
+            conn = new MySqlConnection("Server=localhost;Database=test;Username=root;Password=123209");
+            conn.Open();
+            if (conn.State.ToString()=="Open")
+            {
+                ViewBag.a = "成功";
+                return View();
+            }
+            ViewBag.a = "失败";
+                return View();
+
+        }
+    }
+}
---
+++ WebApplication4/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Class1
+    {
+        int id { get; set; }
+        string name { get; set; }
+    }
+}
