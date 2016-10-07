238c8fe552dfba1f7687d659739a4900db321be6
胡不余 <5509993810@163.com>
2016-10-07 15:22:55 +0800

    创建mysqlcommand对象，进行插入更新或者删除数据

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -22,7 +22,16 @@ namespace WebApplication4.Controllers
             conn.Open();
             if (conn.State.ToString()=="Open")
             {
-                ViewBag.a = "成功";
+                //创建mysqlcommand对象，进行插入更新或者删除数据
+                MySqlCommand com = new MySqlCommand();
+                com.Connection = conn;
+                com.CommandText = "insert into newtable1 values(0,'wang'),(0,'fang');";
+                com.CommandText += "update newtable1 set name='ping' where id=2";
+                //i 表示影响行数
+                int i = com.ExecuteNonQuery();          
+                ViewBag.a =i;
+
+                //ViewBag.a = "成功";
                 return View();
             }
             ViewBag.a = "失败";
